af174fbaa1c3d746cd842150ffcb572fb39db163
Marshal-AM <98259766+Marshal-AM@users.noreply.github.com>
2025-04-16 06:54:19 +0000

    Update server.js

--- indexers/server.js
+++ indexers/server.js
@@ -24,7 +24,7 @@ const options = {
   transports: ["websocket", "polling"],
   path: "/v1/websocket/",
   auth: {
-    apiKey: "p4CtuYObYH1xoB0eWsz09JbFSVa6gdkB",
+    apiKey: "xxxxxxxxxxxxxxxxxx",
   },
   query: {
     protocol: "base",
